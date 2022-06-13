f89c7a178fe1b7b0ef6a24dedcee0f88ba7cfd5e
dvr.rafa@gmail.com <dvr.rafa@gmail.com>
2022-06-13 09:16:56 -0300

    deleteById e findById

--- back-end/src/main/java/com/avaliacao/cast/avaliacaocast/controllers/MedicoController.java
+++ back-end/src/main/java/com/avaliacao/cast/avaliacaocast/controllers/MedicoController.java
@@ -2,6 +2,7 @@ package com.avaliacao.cast.avaliacaocast.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.avaliacao.cast.avaliacaocast.dto.MedicoGetResponse;
 import com.avaliacao.cast.avaliacaocast.dto.MedicoPostRequest;
@@ -12,7 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -49,6 +52,40 @@ public class MedicoController {
 			response.add(item);
 		}
 		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+	
+	@GetMapping(value = ENDPOINT + "/{idMedico}")
+	public ResponseEntity<?> findById(@PathVariable("idMedico")Long idMedico){
+		Optional<Medico> item = medicoRepository.findById(idMedico);
+		if (item.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Médico não encontrado");
+		} else {
+			MedicoGetResponse response = new MedicoGetResponse();
+			Medico medico = item.get();
+			response.setIdMedico(medico.getIdMedico());
+			response.setNome(medico.getNome());
+			response.setCrm(medico.getCrm());
+			response.setTelefone(medico.getTelefone());
+			response.setTipo(medico.getTipo());
+			return ResponseEntity.status(HttpStatus.OK).body(response);
+		}
+	}
+	
+	@DeleteMapping(value = ENDPOINT + "/{idMedico}")
+	public ResponseEntity<String>deleteById(@PathVariable("idMedico")Long idMedico){
+		try {
+			Optional<Medico>item = medicoRepository.findById(idMedico);
+			if (item.isEmpty()) {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado");
+			} else {
+				Medico medico = item.get();
+				medicoRepository.delete(medico);
+				return ResponseEntity.status(HttpStatus.OK).body("Produto deletado com sucesso");
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: "+ e.getMessage());
+		}
+		
 	}
 	
 }
