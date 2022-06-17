8f3d9104072df5ba1fa064a6b06373d5aa62496d
dvr.rafa@gmail.com <dvr.rafa@gmail.com>
2022-06-17 12:56:10 -0300

    validação com senha e token API

---
+++ back-end/src/main/java/com/avaliacao/cast/avaliacaocast/dto/AccountPostRequest.java
@@ -0,0 +1,14 @@
+package com.avaliacao.cast.avaliacaocast.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AccountPostRequest {
+	
+	private String nome;
+	private String login;
+	private String senha;
+
+}
---
+++ back-end/src/main/java/com/avaliacao/cast/avaliacaocast/dto/LoginPostRequest.java
@@ -0,0 +1,13 @@
+package com.avaliacao.cast.avaliacaocast.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginPostRequest {
+	
+	private String login;
+	private String senha;
+
+}
